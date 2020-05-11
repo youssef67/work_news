eddeffe4956a113e44f5db06c484cc4fca3281d9
Youssef Moudni <youssefmoudni@macbook-air-de-youssef.home>
2020-05-11 15:29:30 +0200

    fix comment

--- lib/vendors/Entity/News.php
+++ lib/vendors/Entity/News.php
@@ -1,72 +1,92 @@
 <?php
 namespace Entity;
-
+ 
 use \OCFram\Entity;
-
+ 
 class News extends Entity
 {
-    protected $auteur;
-    protected $titre;
-    protected $contenu;
-    protected $dateAjout;
-    protected $dateModif;
-
-    const AUTEUR_INVALIDE = 1;
-    const TITRE_INVALIDE = 2;
-    const CONTENU_INVALIDE = 3;
-
-    public function isValid()
+  protected $auteur,
+            $titre,
+            $contenu,
+            $dateAjout,
+            $dateModif;
+ 
+  const AUTEUR_INVALIDE = 1;
+  const TITRE_INVALIDE = 2;
+  const CONTENU_INVALIDE = 3;
+ 
+  public function isValid()
+  {
+    return !(empty($this->auteur) || empty($this->titre) || empty($this->contenu));
+  }
+ 
+ 
+  // SETTERS //
+ 
+  public function setAuteur($auteur)
+  {
+    if (!is_string($auteur) || empty($auteur))
     {
-        return !(empty($this->auteur) || empty($this->titre) || empty($this->contenu));
+      $this->erreurs[] = self::AUTEUR_INVALIDE;
     }
-
-    // SETTERS
-
-    public function setAuteur($auteur)
+ 
+    $this->auteur = $auteur;
+  }
+ 
+  public function setTitre($titre)
+  {
+    if (!is_string($titre) || empty($titre))
     {
-        if (!is_string($auteur) || empty($auteur))
-        {
-            $this->erreurs[] = self::AUTEUR_INVALIDE;
-        }
-
-        $this->auteur = $auteur;
+      $this->erreurs[] = self::TITRE_INVALIDE;
     }
-
-    public function setTitre($titre)
+ 
+    $this->titre = $titre;
+  }
+ 
+  public function setContenu($contenu)
+  {
+    if (!is_string($contenu) || empty($contenu))
     {
-        if (!is_string($titre) || empty($titre))
-        {
-            $this->erreurs[] = self::TITRE_INVALIDE;
-        }
-
-        $this->titre = $titre;
+      $this->erreurs[] = self::CONTENU_INVALIDE;
     }
-
-    public function setContenu($contenu)
-    {
-        if (!is_string($contenu) || empty($contenu))
-        {
-            $this->erreurs[] = self::CONTENU_INVALIDE;
-        }
-
-        $this->contenu = $contenu;
-    }
-
-    public function setDateAjout(\DateTime $dateAjout)
-    {
-        $this->dateAjout = $dateAjout;
-    }
-
-    public function setDateMOdif(\DateTime $dateModif)
-    {
-        $this->dateModif = $dateModif;
-    }
-
-    // GETTERS //
-
-    public function auteur() { return $this->auteur; }
-    public function titre() { return $this->titre; }
-    public function contenu() { return $this->contenu; }
-    public function dateAjout() { return $this->dateAjout; }
-    public function dateModif() { return $this->dateModif; }
+ 
+    $this->contenu = $contenu;
+  }
+ 
+  public function setDateAjout(\DateTime $dateAjout)
+  {
+    $this->dateAjout = $dateAjout;
+  }
+ 
+  public function setDateModif(\DateTime $dateModif)
+  {
+    $this->dateModif = $dateModif;
+  }
+ 
+  // GETTERS //
+ 
+  public function auteur()
+  {
+    return $this->auteur;
+  }
+ 
+  public function titre()
+  {
+    return $this->titre;
+  }
+ 
+  public function contenu()
+  {
+    return $this->contenu;
+  }
+ 
+  public function dateAjout()
+  {
+    return $this->dateAjout;
+  }
+ 
+  public function dateModif()
+  {
+    return $this->dateModif;
+  }
 }
--- lib/vendors/Model/CommentsManager.php
+++ lib/vendors/Model/CommentsManager.php
@@ -1,29 +1,67 @@
 <?php
 namespace Model;
-
-use Entity\Comment;
-use OCFram\Manager;
-
+ 
+use \OCFram\Manager;
+use \Entity\Comment;
+ 
 abstract class CommentsManager extends Manager
 {
-    abstract protected function add(Comment $add);
-
-    public function save(Comment $comment)
+  /**
+   * Méthode permettant d'ajouter un commentaire.
+   * @param $comment Le commentaire à ajouter
+   * @return void
+   */
+  abstract protected function add(Comment $comment);
+ 
+  /**
+   * Méthode permettant de supprimer un commentaire.
+   * @param $id L'identifiant du commentaire à supprimer
+   * @return void
+   */
+  abstract public function delete($id);
+ 
+  /**
+   * Méthode permettant de supprimer tous les commentaires liés à une news
+   * @param $news L'identifiant de la news dont les commentaires doivent être supprimés
+   * @return void
+   */
+  abstract public function deleteFromNews($news);
+ 
+  /**
+   * Méthode permettant d'enregistrer un commentaire.
+   * @param $comment Le commentaire à enregistrer
+   * @return void
+   */
+  public function save(Comment $comment)
+  {
+    if ($comment->isValid())
     {
-        if ($comment->isValid())
-        {
-            $comment->isNew() ? $this->add($comment) : $this->modify($comment);
-        }
+      $comment->isNew() ? $this->add($comment) : $this->modify($comment);
     }
-
-    abstract public function getListOf($news);
-
-    abstract public function modify(Comment $comment);
-
-    abstract public function get($id);
-
-    abstract public function delete($id);
-
-    abstract public function deleteFromNews($newsId);
+    else
+    {
+      throw new \RuntimeException('Le commentaire doit être validé pour être enregistré');
+    }
+  }
+ 
+  /**
+   * Méthode permettant de récupérer une liste de commentaires.
+   * @param $news La news sur laquelle on veut récupérer les commentaires
+   * @return array
+   */
+  abstract public function getListOf($news);
+ 
+  /**
+   * Méthode permettant de modifier un commentaire.
+   * @param $comment Le commentaire à modifier
+   * @return void
+   */
+  abstract protected function modify(Comment $comment);
+ 
+  /**
+   * Méthode permettant d'obtenir un commentaire spécifique.
+   * @param $id L'identifiant du commentaire
+   * @return Comment
+   */
+  abstract public function get($id);
 }
-       
